c83b55191e834e94a82ccd60226426311e0fddd9
adamsch60 <adamschweitzer1@gmail.com>
2016-10-24 18:37:16 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,20 @@
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
+/*
+   chrome.fileBrowserHandler.onExecute.addListener(function(id, details) {
+        if (id == 'upload') {
+          var fileEntries = details.entries;
+          for (var i = 0, entry; entry = fileEntries[i]; ++i) {
+            entry.file(function(file) {
+              // send file somewhere
+            });
+          }
+        }
+      });
+*/
 
 document.addEventListener('DOMContentLoaded', function() {
+
     // Put the image URL in Google search.     
 });
