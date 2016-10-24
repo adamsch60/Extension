4e4007127973578a5a47cb5b2cd50b75ddc51c9e
FightingEagle20 <patrick.nanys2000@gmail.com>
2016-10-24 20:41:18 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,30 +2,15 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
-
-
 chrome.alarms.onAlarm.addListener(function(alarm){
 chrome.downloads.download({url:alarm.name});
 })
-
-
-
-
 
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('save');
     checkPageButton.addEventListener('click', function() {
         var link = document.getElementById('link').value;
         var time = document.getElementById('time').value;
-        chrome.alarms.create(link, {
-        delayInMinutes: time});
+        chrome.alarms.create(link, {delayInMinutes: time});
     });
-
-    // onClick's logic below:
-
-    
-
-
-        
-      // Put the image URL in Google search.     
 });
