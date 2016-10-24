858e4d96d4023a89deb671ff6aa3a53549b0c067
adamsch60 <adamschweitzer1@gmail.com>
2016-10-24 19:15:12 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -5,6 +5,6 @@ function renderStatus(statusText) {
 
 
 document.addEventListener('DOMContentLoaded', function() {
-chrome.downloads.download(object{url=" https://developer.chrome.com/extensions/examples/tutorials/getstarted/manifest.json"}, function callback);
+chrome.downloads.download({url:" https://developer.chrome.com/extensions/examples/tutorials/getstarted/manifest.json"});
     // Put the image URL in Google search.     
 });
