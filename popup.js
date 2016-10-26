d3c79b11883dff403faa86a3c11bf0ce5baee9b7
adamsch60 <adamschweitzer1@gmail.com>
2016-10-26 17:09:15 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,10 +2,11 @@ var list_url=[];
 var list_name=[];
 
 chrome.alarms.onAlarm.addListener(function(alarm){
-    
-chrome.downloads.download({url:list_url[list_name.findIndex(alarm.name)]});
-list_url.splice(list_name.findIndex(alarm.name),1);
-list_name.splice(list_name.findIndex(alarm.name),1);
+  console.log(list_name.indexOf(alarm.name)+','+alarm.name+','+list_name+','+list_url);
+  
+chrome.downloads.download({url:list_url[list_name.indexOf(alarm.name)]});
+list_url.splice(list_name.indexOf(alarm.name),1);
+list_name.splice(list_name.indexOf(alarm.name),1);
     
     /**/
 })
@@ -15,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
     checkPageButton.addEventListener('click', function() {
         var name = document.getElementById('name').value;
         var link = document.getElementById('link').value;
-        var time = document.getElementById('time').value;
+        var time = Number(document.getElementById('time').value);
         chrome.alarms.create(name, {delayInMinutes: time});
       list_url.push(link);
        list_name.push(name);
