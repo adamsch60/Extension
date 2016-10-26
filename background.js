a7f23ee885991aa8e4507ce9ef4a24e3c263c8e1
adamsch60 <adamschweitzer1@gmail.com>
2016-10-26 17:34:25 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
 
+
+chrome.alarms.onAlarm.addListener(function(alarm){
+  chrome.downloads.download({url:list_url[list_name.indexOf(alarm.name)]});
+  list_url.splice(list_name.indexOf(alarm.name),1);
+  list_name.splice(list_name.indexOf(alarm.name),1);
+})
