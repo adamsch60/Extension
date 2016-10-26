457dab50819f4f43b4c2258f880195b5a70722ed
adamsch60 <adamschweitzer1@gmail.com>
2016-10-26 17:34:06 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,6 @@
 var list_url=[];
 var list_name=[];
 
-chrome.alarms.onAlarm.addListener(function(alarm){
-  chrome.downloads.download({url:list_url[list_name.indexOf(alarm.name)]});
-  list_url.splice(list_name.indexOf(alarm.name),1);
-  list_name.splice(list_name.indexOf(alarm.name),1);
-})
 
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('save');
