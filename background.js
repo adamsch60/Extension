2257577e593325360b6d152a85cd0ef2dbcbb4b8
adamsch60 <adamschweitzer1@gmail.com>
2016-10-26 17:38:35 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
+var list_url=[];
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.msg == "list_url") {
+        sendResponse(list_url);
+        return true;
+    }
+});
+var list_name=[];
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.msg == "list_name") {
+        sendResponse(list_name);
+        return true;
+    }
+});
 
 
 chrome.alarms.onAlarm.addListener(function(alarm){
